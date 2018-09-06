ce28b94bb703ef4b675136808995bb2fad335102
Павел Сидоров <pasha.sidorov@gmail.com>
2018-09-06 11:50:15 +0300

    Missing file + added ignore docs

---
+++ StocksAnalyzer/Helpers/Enums.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StocksAnalyzer.Helpers
+{
+	[Flags]
+	enum SortingModes
+	{
+		Metric,
+		Coefficeint,
+		PositionAll,
+		PositionMetric,
+		PositionCoef
+	}
+}
